e0072c76634b9ddc42aba6f51d54775ce7d2c142
Antonio de Pádua <antonio.junior.h@gmail.com>
2023-03-28 19:55:45 -0300

    feat: adding product model

---
+++ src/model/ProductModel.php
@@ -0,0 +1,15 @@
+<?php
+
+class ProductModel
+{
+    public $tableName = 'product';
+
+    public $description = 'description';
+    public $productTypeId = 'product_type_id';
+
+
+    public function __construct()
+    {
+      
+    }
+}
